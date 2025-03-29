fbe139a11f02730f80e182b354a4f11c7ab9c3ba
SanthiyaPeriyasamy245 <113085174+SanthiyaPeriyasamy245@users.noreply.github.com>
2025-03-29 09:59:53 +0530

    Add files via upload

---
+++ RollingDice.py
@@ -0,0 +1,17 @@
+
+# Rolling Dice
+import random
+while(True):
+    
+ 
+    choice=input('Roll the dice ? y/n').lower()
+    if(choice=='y'):
+         dice1=random.randint(1,6)
+         dice2=random.randint(1,6)
+         print(f'({dice1},{dice2})')
+    elif (choice=='n'):
+         print('Thanks for Playing !')
+         break
+    else:
+         print('Invalid choice')
+ 
